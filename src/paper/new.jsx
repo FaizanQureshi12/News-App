f653ee49cce3222e7ffc9f95df5af3875bbef177
Faizan <qfaizan489@gmail.com>
2022-10-23 21:21:57 +0500

    C2

--- src/paper/new.jsx
+++ src/paper/new.jsx
@@ -1,13 +1,38 @@
-   import{useState} from 'react' 
+   import{useState,useEffect} from 'react' 
    import './new.css'
    import axios from 'axios'
    import moment from 'moment'
    
-//    const axios = require('axios').default
 
    function Paper (){
     const [data, setData]=useState([]);
     const [query, setQuery]=useState('');
+    const [isLoading, setIsLoading]=useState(false);
+
+        useEffect(()=>{
+        function getTrendingNews(){
+
+            const options = {
+                method: 'GET',
+                url: 'https://bing-news-search1.p.rapidapi.com/news',
+                params: {safeSearch: 'Off', textFormat: 'Raw'},
+                headers: {
+                  'X-BingApis-SDK': 'true',
+                  'X-RapidAPI-Key': '77bd12c926msh288972a4c534cedp1d8669jsnc3609c71780b',
+                  'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com'
+                }
+              };
+
+            axios.request(options).then(function (response) {
+                console.log(response.data);
+                setData(response.data.value)
+            }).catch(function (error) {
+                console.error(error);
+            });
+        }
+        getTrendingNews();
+    },[]
+    )
 
     const getNews =(e)=>{
         e.preventDefault();
@@ -23,35 +48,51 @@
           }
         };
         
-        axios.request(options).then(function (response) {
+        setIsLoading(true)
+        axios
+        .request(options).then(function (response) {
+            setIsLoading(false)  
             console.log(response.data.value);
             setData(response.data.value)
         }).catch(function (error) {
+             setIsLoading(false)
             console.error(error);
         });
     }
 
-
     return(
         <div>
-<form onSubmit={getNews}>
+    <form onSubmit={getNews}>
     <input type="text" placeholder='Enter Your City Name' 
      onChange={(e)=>
      {setQuery(e.target.value)} }  id="" />
     <button type='submit'>Get News</button>
+        </form>
+        <div>
 
-</form>
-<div>
+    {(isLoading)? "Loading...":""}
     {data.map(eachPost=> (
-    <div className='post' key={eachPost.name}>
-        <h1>{eachPost.name} </h1>
-        <span>{moment(eachPost.datePublished)
-        .format('Do MMMM YYYY, h:mm a')}</span>
-        <h3>{eachPost.description} </h3>
-        </div>))}
-</div>
+    <div className='post' key={eachPost?.name}>
 
-        </div>
+        <h2>
+            <a href={eachPost?.url}
+            target='_blank' rel='noreferrer'> 
+            {eachPost?.name}</a>  
+        </h2>
+        <span>
+            {moment(eachPost?.datePublished)
+        .format('Do MMMM YYYY, h:mm a')}
+        </span>
+
+        <h5>{eachPost?.description} </h5>
+        <img src={
+            eachPost?.image?.thumbnail?.contentUrl
+            .replace("&pid=News",'')
+            .replace('pid=News&','')
+            .replace('pid=News','')} alt="" />
+           </div>))}
+       </div>
+ </div>
     );
    }
    export default Paper;
