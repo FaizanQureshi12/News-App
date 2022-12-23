3d4324b97822cbe916df29acdcbc7808a0f3f737
Faizan <qfaizan489@gmail.com>
2022-12-23 20:11:19 +0500

    response

--- src/paper/new.jsx
+++ src/paper/new.jsx
@@ -1,27 +1,27 @@
-   import{useState,useEffect} from 'react' 
-   import './new.css'
-   import axios from 'axios'
-   import moment from 'moment'
-          
-   function Paper (){
-    const [data, setData]=useState([]);
-    const [query, setQuery]=useState('');
-    const [isLoading, setIsLoading]=useState(false);
+import { useState, useEffect } from 'react'
+import './new.css'
+import axios from 'axios'
+import moment from 'moment'
+
+function Paper() {
+    const [data, setData] = useState([]);
+    const [query, setQuery] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
 
     //class code
-        useEffect(()=>{
-        function getTrendingNews(){
+    useEffect(() => {
+        function getTrendingNews() {
 
             const options = {
                 method: 'GET',
                 url: 'https://bing-news-search1.p.rapidapi.com/news',
-                params: {safeSearch: 'Off', textFormat: 'Raw'},
+                params: { safeSearch: 'Off', textFormat: 'Raw' },
                 headers: {
-                  'X-BingApis-SDK': 'true',
-                  'X-RapidAPI-Key': '77bd12c926msh288972a4c534cedp1d8669jsnc3609c71780b',
-                  'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com'
+                    'X-BingApis-SDK': 'true',
+                    'X-RapidAPI-Key': '77bd12c926msh288972a4c534cedp1d8669jsnc3609c71780b',
+                    'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com'
                 }
-              };
+            };
 
             axios.request(options).then(function (response) {
                 console.log(response.data);
@@ -31,74 +31,73 @@
             });
         }
         getTrendingNews();
-    },[]
+    }, []
     )
 
-    const getNews =(e)=>{
+    const getNews = (e) => {
         e.preventDefault();
 
         const options = {
-          method: 'GET',
-          url: 'https://bing-news-search1.p.rapidapi.com/news/search',
-          params: {q: query, freshness: 'Day', textFormat: 'Raw', safeSearch: 'Off'},
-          headers: {
-            'X-BingApis-SDK': 'true',
-            'X-RapidAPI-Key': '77bd12c926msh288972a4c534cedp1d8669jsnc3609c71780b',
-            'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com'
-          }
+            method: 'GET',
+            url: 'https://bing-news-search1.p.rapidapi.com/news/search',
+            params: { q: query, freshness: 'Day', textFormat: 'Raw', safeSearch: 'Off' },
+            headers: {
+                'X-BingApis-SDK': 'true',
+                'X-RapidAPI-Key': '77bd12c926msh288972a4c534cedp1d8669jsnc3609c71780b',
+                'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com'
+            }
         };
-        
+
         setIsLoading(true)
         axios
-        .request(options).then(function (response) {
-            setIsLoading(false)  
-            console.log(response.data.value);
-            setData(response.data.value)
-        }).catch(function (error) {
-             setIsLoading(false)
-            console.error(error);
-        });
+            .request(options).then(function (response) {
+                setIsLoading(false)
+                console.log(response.data.value);
+                setData(response.data.value)
+            }).catch(function (error) {
+                setIsLoading(false)
+                console.error(error);
+            });
     }
 
-    return(
-
-                    <div>
-                <div className='navbar'>
-    <form onSubmit={getNews}>
-    <input type="text"  placeholder='Enter Your City Name' 
-     onChange={(e)=>
-     {setQuery(e.target.value)} }  id="inp" />
-    <button className='btn' type='submit'>Get News</button>
-        </form>
-        </div>
+    return (
 
         <div>
-    {(isLoading)? "Loading...":""}
-    <div className='fullpost'>
-    {data.map(eachPost=> (
-    <div className='post' key={eachPost?.name}>
+            <div className='navbar'>
+                <form onSubmit={getNews}>
+                    <input type="text" placeholder='Enter Your City Name'
+                        onChange={(e) => { setQuery(e.target.value) }} id="inp" />
+                    <button className='btn' type='submit'>Get News</button>
+                </form>
+            </div>
 
-        <h2>
-            <a href={eachPost?.url}
-            target='_blank' rel='noreferrer'> 
-            {eachPost?.name}</a>  
-        </h2>
-        <span>
-            {moment(eachPost?.datePublished)
-        .format('Do MMMM YYYY, h:mm a')}
-        </span>
+            <div>
+                {(isLoading) ? "Loading..." : ""}
+                <div className='fullpost'>
+                    {data.map(eachPost => (
+                        <div className='post' key={eachPost?.name}>
 
-        <h5>{eachPost?.description} </h5>
-        <img src={
-            eachPost?.image?.thumbnail?.contentUrl
-            .replace("&pid=News",'')
-            .replace('pid=News&','')
-            .replace('pid=News','')}  className='img' alt="" />
-           </div>))}
-       </div>
+                            <h3>
+                                <a href={eachPost?.url}
+                                    target='_blank' rel='noreferrer'>
+                                    {eachPost?.name}</a>
+                            </h3>
+                            <span>
+                                {moment(eachPost?.datePublished)
+                                    .format('Do MMMM YYYY, h:mm a')}
+                            </span>
 
-       </div>
- </div>
+                            <h6>{eachPost?.description} </h6>
+                            <img src={
+                                eachPost?.image?.thumbnail?.contentUrl
+                                    .replace("&pid=News", '')
+                                    .replace('pid=News&', '')
+                                    .replace('pid=News', '')} className='img' alt="" />
+                        </div>))}
+                </div>
+
+            </div>
+        </div>
     );
-   }
-   export default Paper;
+}
+export default Paper;
